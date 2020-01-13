6abca747ec1a84fdd047ee159dfa46dec093fe13
LevPewPew <levtoth1988@gmail.com>
2020-01-13 17:33:09 +1100

    created google map component

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -1,1 +1,20 @@
 import React from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import '../styles/GoogleMap.css';
+
+function GoogleMap(props) {
+  const {google, latitude, longitude} = props;
+
+  return (
+    <Map
+      google={google}
+      zoom={8}
+      initialCenter={{latitude, longitude}}
+    >
+      <Marker position={{latitude, longitude}} />
+    </Map>
+    
+  );
+}
+
+export default GoogleApiWrapper({apiKey: 'AIzaSyApnc2TgJNtG3sxgKOHbBldQzDEeffYbGg'})(GoogleMap);
