7f81c1e114225cd09a1c70f0348d39086aabd165
Melissa Bykersma <melissabykersma@gmail.com>
2020-01-13 15:02:33 +1100

    Added base files for Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-const App = () => {
-    return(
-        <div>
-            <p>This is the main app page</p>
-        </div>
-    )
-}
+import ReactRouter from './router/ReactRouter'
 
-render(<App />, document.getElementById('root'))
+render(<ReactRouter />, document.getElementById('root'))
