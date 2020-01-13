a41eff250ede7348bb9b34cdf219c3e31665ca8a
Mark <markke4@outlook.com>
2020-01-14 09:22:00 +1100

    routes

--- src/router/ReactRouter.js
+++ src/router/ReactRouter.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Search from '../pages/Search';
+import NotFoundPage from '../pages/NotFound';
 
 const Router = () => {
     return(
         <div>
-            <h1>Hello</h1>
+            <BrowserRouter>
+                <Navbar />
+                <Switch>
+                    <Route path="/" component={Home} exact={true} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                    <Route path="/search" component={Search} />
+                    
+                    <Route component={NotFoundPage} />
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
+
 
 export default Router
