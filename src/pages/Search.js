d564d0752120c80261fcbc95c94c5bb5daadaffa
Nhan Dang <nhantdang@gmail.com>
2020-01-13 16:06:41 +1100

    search page updated

---
+++ src/pages/Search.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {render} from 'react-dom'
+
+import ReactRouter from './router/ReactRouter'
+
+class Search extends React.Component {
+
+  state = {
+    cafesList = [],
+    milkSearch = ' ',
+    cafeSearch = ' '
+  }
+  
+  componentDidMount() {
+    //load all cafes here
+  }
+
+
+
+  render() {
+    return( 
+// Search form goes here
+
+    )
+  }
+}
+
+export default Search;
