309b4c2805df7e93e66b586086d1d9be3cfeccf2
Max Kelly <max@revelthat.com>
2020-01-13 16:08:43 +1100

    changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-<<<<<<< HEAD
-import Router from './router/ReactRouter'
-=======
 // React Router Imports
 import Router from './router/ReactRouter'
 
->>>>>>> 209506be6c7bf078d769923d629d8568d11972d8
 
 render(<Router />, document.getElementById('root'))
