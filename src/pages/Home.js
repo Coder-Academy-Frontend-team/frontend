7dcc5effc38c2be66213bc9a0bc838bc93bcc6d7
Mark <markke4@outlook.com>
2020-01-13 15:55:59 +1100

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,15 @@
 import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      {/* <HomeHeroBanner /> */}
+      <h1>Latest Reviews</h1>
+      <div>
+        {/* {map and call the components for each one, passing in the review object} */}
+      </div>
+    </>
+  )
+};
+
+export default Home;
