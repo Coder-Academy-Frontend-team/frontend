7d149c9a86a30d7b89f9031ce5d8d9c061719bbe
Max Kelly <max@revelthat.com>
2020-01-13 16:42:43 +1100

    added hooks to coffee profile

--- src/pages/CoffeeProfile.js
+++ src/pages/CoffeeProfile.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
-class CoffeeProfile extends React.Component {
-  render () {
-    return (
-      <div>
-        <h1> Cafe Name </h1>
-      </div>
-    ) 
-  }
+// HOOKS
+import React, { useState, useEffect } from 'react';
+
+const CoffeeProfile = () => {
+
+  const [reviews, setReview] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const REACT_APP_BACKEND_URL = "enter URL here";
+      const response = await fetch(REACT_APP_BACKEND_URL);
+      const data = await response.json();
+
+      setReviews([
+        ...reviews,
+        ...data
+      ]);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1> Coffee Profile </h1>
+
+    </div>
+  )
 };
 
 
