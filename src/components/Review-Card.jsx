9530dd0963817586f20e937d1f68bbd4207ed890
Carl Davidson <carl.davidson@hotmail.com>
2020-01-14 11:39:42 +1100

    refactored the card

--- src/components/Review-Card.jsx
+++ src/components/Review-Card.jsx
@@ -1,38 +1,28 @@
 import React from 'react'
 import '../styles/Review-Card.scss';
 
-  function getAllReviews() {
-      return {
-          array.forEach(element => {
+//   function getAllReviews() {
+//       return {
+//           array.forEach(element => {
               
-          });
-      }
-  }
+//           });
+//       }
+//   }
 class Review_Card extends React.Component{
     
-    getRating() {
+    // getRating() {
         
-        return
-    }
+    //     return
+    // }
     
     render() {
         const { coffeeType, milkType, photo, rating, comment } = this.props
         return(
             <div className="reviewCard">
-                <h1>Title</h1>
-        <div>`Coffee: ${coffeeType} milk: ${milkType}`</div>
+                <p>`Coffee: ${coffeeType} milk: ${milkType}`</p>
                 <div>{rating}</div>
                 <img src="#" className="reviewPhoto">{photo}</img>
                 <aside>{comment}</aside>
-                {/* {coffeeType}
-                
-                {milkType}
-                
-                {photo}
-                
-                {rating}
-                
-                {comment} */}
             </div>
         )
     }
