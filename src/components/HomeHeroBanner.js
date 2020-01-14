9570aa397a7273668f5fea540252263fec51bb81
Mark Tomlinson <mark.tomlinson3@gmail.com>
2020-01-14 11:42:49 +1100

    added HomeHero component and HomeHero css

---
+++ src/components/HomeHeroBanner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../styles/HomeHeroBanner.css";
+
+const HomeHeroBanner = () => {
+	return (
+		<div className="header">
+			<div className="text-box">
+				
+			</div>
+
+		</div>
+
+	)
+
+}
+
+export default HomeHeroBanner
