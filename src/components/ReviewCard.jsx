5019c09c95b00ccfb93ac6674462cd8e8d78f887
Carl Davidson <carl.davidson@hotmail.com>
2020-01-15 09:09:29 +1100

    begun rendering data

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -2,26 +2,11 @@ import React from 'react'
 import '../styles/Review-card.scss';
 
 class ReviewCard extends React.Component{
-    state = {
-            data: null
-            }
-
-            async componentDidMount() {
-                const response = await fetch('https://coffee-back-end.herokuapp.com/reviews')
-                const data = await response.json()
-                this.setState({
-                    data
-                })
-            }
-
 
     render() {
-        const { } = this.state.data
+        const {coffeeType, milkType, photo, rating, comment, index} = this.props
         return(
             <div className="reviewCard">
-                {data? data.map((coffeeType, milkType, photo, rating, comment, index) => {
-                    return(
-                        <div>
                         <p>Coffeetype{coffeeType} || milktype{milkType}</p>
                         <p>Rating{rating}</p>
                         <img 
@@ -31,11 +16,8 @@ class ReviewCard extends React.Component{
                         alt=" " />
                         <aside>{comment}</aside>
                         </div>
-                    )
-                }) : null}
-            </div>
-        )
-    }
+                )
+                }
 }
 
 export default ReviewCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import HomeHeroBanner from '../components/HomeHeroBanner'
 import Navbar from '../components/Navbar';
-import ReviewCard from './ReviewCard';
+import ReviewCard from '../components/ReviewCard.jsx';
 
 const Home = () => {
   const [coffeeShops, setCoffeeShops] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const REACT_APP_BACKEND_URL = "enter url here";
+      const REACT_APP_BACKEND_URL = "https://coffee-back-end.herokuapp.com/";
       const response = await fetch(REACT_APP_BACKEND_URL);
       const data = await response.json();
       // console.log(data);
@@ -26,7 +26,7 @@ const Home = () => {
         <HomeHeroBanner />
         <h1>Latest Reviews</h1>
         <div>
-        <ReviewCard />
+        <ReviewCard props={data}/>
             {/* {map and call the components for each one, passing in the review object} */}
         </div>
     </>
