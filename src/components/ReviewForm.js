73ca508c0dcb466a1e89be047fbd18f85ed23d34
Pankaj Pawar <pankaj.pawar1986@yahoo.com.au>
2020-01-14 11:04:46 +1100

    Review-form

---
+++ src/components/ReviewForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useForm } from 'react-hook-form'
+
+export default function ReviewForm()
+ {
+  const { register, handleSubmit, watch, errors } = useForm()
+  const onSubmit = data => { console.log(data) }
+  
+  return (
+
+    // "handleSubmit" will validate your inputs before invoking "onSubmit"
+    <form onSubmit={handleSubmit(onSubmit)}>
+    {/* register your input into the hook by invoking the "register" function */}
+      <input name="example" defaultValue="test" ref={register} />
+      
+      {/* include validation with required or other standard HTML validation rules */}
+      <input name="exampleRequired" ref={register({ required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+      
+      <input type="submit" />
+    </form>
+  )
+  
+}
+
+export default ReviewForm
