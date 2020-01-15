6596720d01bbc8b6d4481ea94a9b097e16a1da62
Kyle46220 <kyle@smithandthomas.com.au>
2020-01-15 11:08:42 +1100

    search box to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,13 @@ import { NavLink } from 'react-router-dom';
 
 import '../styles/Navbar.css'
 import logo from '../assets/logo.png'
-
+import SeacrhBar from './SearchBar';
 const Navbar = () => {
   return (
     <nav>
       <div className='links'>
         <div>
+          <SearchBar />
           <NavLink exact={true} to="/">
             <img src={logo} alt='logo' className='logo'/>
           </NavLink>
@@ -16,6 +17,7 @@ const Navbar = () => {
         <div>
           <NavLink activeClassName="is-active" exact={true} to="/login" className='individual-link'>Login</NavLink>
           <NavLink activeClassName="is-active" to="/search" className='individual-link'>Search</NavLink>
+          
         </div>
       </div>
     </nav>
